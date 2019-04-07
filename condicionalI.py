bb213f41aab83d040998f5ed3a4490678c9f1484
KarinaHeras <karinaheras1847@gmail.com>
2019-04-07 13:53:04 +0200

    faltab un print al final para que funcionara , mia culpa

--- condicionalI.py
+++ condicionalI.py
@@ -1,16 +1,16 @@
-salario_presidente=int(input("Introduce Salario Presidente"))
+salario_presidente=int(input("Introduce Salario Presidente "))
 print("Salario Presidente: "+ str(salario_presidente))
 
-salario_director=int(input("Introduce Salario Director"))
+salario_director=int(input("Introduce Salario Director "))
 print("Salario director: "+ str(salario_director))
 
-salario_jefe_area=int(input("Introduce Salario Jefe de area"))
+salario_jefe_area=int(input("Introduce Salario Jefe de area "))
 print("Salario jefe de area: "+ str(salario_jefe_area))
 
-salario_administrativo=int(input("Introduce Salario ministrativo"))
+salario_administrativo=int(input("Introduce Salario ministrativo "))
 print("Salario administrativo: "+ str(salario_administrativo))
 
 if salario_administrativo<salario_jefe_area<salario_director<salario_presidente:
     print("Todo funciona correctamente")
 else:
-    ("En esta empresa hay una lewinsky jajajaja")    
+  print("En esta empresa hay una lewinsky jajajaja")    
