c0d34ebcbec0101d97605ec65488c0a05e6b69f7
KarinaHeras <karinaheras1847@gmail.com>
2019-04-07 14:58:43 +0200

    bubles primer ejemplo

---
+++ bucles-for.py
@@ -0,0 +1,11 @@
+for estaciones_año in["primaveras","verano","otoño","invierno"]:
+    print(estaciones_año)
+
+
+
+## otro ejemplo de bucles recorreindo string, tipo range, notaciones especiales con print
+
+
+
+
+
--- operadores_and_or.py
+++ operadores_and_or.py
@@ -1,11 +1,11 @@
 print("Programa de becas Año 2019")
-distancia_escuela=int(input("Introduce la distanacia de la escuela en km ")) 
+distancia_escuela=int(input("Introduce la distancia de la escuela en km ")) 
 print(distancia_escuela)
 
 numeros_hermanos=int(input("Introduce el Nº de hermanos en el centro "))
 print(numeros_hermanos)
 
-salario_familiar=int(input("Introdce el salario anual bruto  "))
+salario_familiar=int(input("Introduce el salario anual bruto  "))
 print(salario_familiar)
 
 if distancia_escuela>40 and numeros_hermanos>2 or salario_familiar<=20000:
