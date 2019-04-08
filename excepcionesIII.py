8c4e55f3a5dfd51b751159a1c434f6287f814d19
KarinaHeras <karinaheras1847@gmail.com>
2019-04-08 15:14:08 +0200

    como capturar una excepcion, y  añadir mi propio error

---
+++ excepcionesIII.py
@@ -0,0 +1,17 @@
+import math
+
+def calculaRaiz(num1):
+
+    if num1<0:
+        raise ValueError ("El número no puede ser negativo")
+    else:
+        return math.sqrt(num1)
+opcion1=(int(input("Introduce un número: ")))   
+
+try:
+     print(calculaRaiz(opcion1))   
+
+except ValueError as ErrorDeNumeroNegativo:     
+    print(ErrorDeNumeroNegativo)
+
+print("Programa terminado")
