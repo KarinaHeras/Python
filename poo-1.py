ec710b81a57a5a9917bc002a123c2c9deea52332
KarinaHeras <karinaheras1847@gmail.com>
2019-04-08 18:01:59 +0200

    otro ejemplo de metodo constructor y encapsulador

--- poo-1.py
+++ poo-1.py
@@ -1,24 +1,39 @@
 class Coche():
-    def _init_(self):
-    self.largoChasis=250
-    self.anchoChasis=120
-    self.__ruedas=4   # con __con dos guines bajo encapsulamos una varieble
-    self.enmarcha=False
+    def _init_(self): # metodo constructor init con guion bajo uno delante otro despues
+
+        self.__largoChasis=250
+        self.__anchoChasis=120
+        self.__ruedas=4            # con __con dos guines bajo encapsulamos una varieble
+        self.__enmarcha=False
 
     def arrancar(self,arrancamos):
-        self.enmarcha=arrancamos
-        if(self.enmarcha)
-            return"El coche está en marcha"
+        self.__enmarcha=arrancamos
+        if(self.__enmarcha):
+            chequeo=self.chequeo_interno()
 
-    def estado(self)  
-        if(self.enmarcha):
-            return"El coche esta en marcha"  
+        if(self.__enmarcha and chequeo):
+            return"El coche está en marcha"
+        elif(self.__enmarcha and chequeo==False):
+            return"Algo no va bien en el chequeo"    
+
         else:
             return"el coche esta parado"  
 def estado(self):
-    print("El coche ", self.__ruedas, "ruedas.un ancho de ", self.anchoChasis, "y un largo de",
-     self.largoChasis)
+    print("El coche ", self.__ruedas, "ruedas.un ancho de ", self.__anchoChasis, "y un largo de",
+     self.__largoChasis)
 
+
+def chequeo_interno(self):
+    print("realizando chequeo interno")
+
+    self.gasolina="Ok"
+    self.aceite="Ok"
+    self.puertas="cerradas"
+
+    if (self.gasolina=="Ok" and self.aceite=="Ok" and self.puertas=="cerradas")
+        return True
+    else:
+        return False    
 miCoche=Coche()  
 print(miCoche.arrancar(True))   
 
