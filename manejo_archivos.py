be96545620fe7fa17e0ec9cc444819dd4210c3ab
KarinaHeras <karinaheras1847@gmail.com>
2019-04-09 16:43:06 +0200

    como crear un archivo, como leerlo y como abrirlo y cerrarlo

---
+++ manejo_archivos.py
@@ -0,0 +1,8 @@
+from io import open
+
+archivo_texto=open("archivo.txt","a")
+
+archivo_texto.write("\n es una muy buena ocasion para estudiar python")
+ archivo_texto.close()
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup (
+    name="paqueteCalculo",
+    version="1.0",
+    descripcion="paquete de redondeo y potencia",
+    author="Karina Heras",
+    author_email="karinaheras1847@gmail.com",
+    url="",
+    packages=["calculo","calculo.redondeo_potencia"]
+
+)
